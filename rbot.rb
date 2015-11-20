68161284492e0ad5d949157ee8aae44d2d7f79f2
Aaron Zink <azink@solsar.com>
2015-11-20 13:35:01 -0800

    Removing alias

--- rbot.rb
+++ rbot.rb
@@ -7,9 +7,4 @@ module Rbot
   Dir["./hooks/*.rb"].each {|f| require f}
 end
 
-SlackRubyBot.configure do |config|
-  #config.aliases = Dir["./hooks/*.rb"].map {|f| "!" + f.split("/").last.gsub(".rb", "")} + "rb"
-  config.aliases = ["!"]
-end
-
 Rbot::App.instance.run
