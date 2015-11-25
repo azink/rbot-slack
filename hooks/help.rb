fcc84f9d8d77449e5480a51ed86472c5a24193fd
Aaron Zink <azink@solsar.com>
2015-11-24 16:33:54 -0800

    consolidating simple commands and adding seen/next

--- hooks/help.rb
+++
@@ -1,5 +0,0 @@
-class RBHelp < SlackRubyBot::Commands::Base
-  match(/^!help/i) do |client, data, match|
-    client.message text: "See https://github.com/azink/rbot-slack for more information on available hooks.  Ask Gand for push privileges.", channel: data.channel
-  end
-end
