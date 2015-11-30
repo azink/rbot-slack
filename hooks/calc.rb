2e2ce5d525e7866033d231253029ba57e1f88ac7
Aaron Zink <azink@solsar.com>
2015-11-30 14:52:23 -0800

    calc: removing "bc result"

--- hooks/calc.rb
+++ hooks/calc.rb
@@ -2,7 +2,7 @@
 class Calc < SlackRubyBot::Commands::Base
   match(/^!calc($|\s(?<args>.+$))/i) do |client, data, match|
     out = `echo "#{match[:args]}" | bc -l 2>&1`
-    client.message text: "bc result: #{out}", channel: data.channel
+    client.message text: out, channel: data.channel
   end
 end
 
