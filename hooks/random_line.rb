b60effabb294a1e83562164a82aa2bd4e4a5706a
Aaron Zink <azink@solsar.com>
2015-11-30 15:24:20 -0800

    Fixing random_line path

--- hooks/random_line.rb
+++ hooks/random_line.rb
@@ -4,7 +4,7 @@ class RandomLine < SlackRubyBot::Commands::Base
   hooks = Dir["./db/txt/*.txt"].map {|f| f[/\/(\w+)\./, 1]}
   hooks.each do |hook|
     match(/^!#{hook}($|\s(?<args>.+$))/i) do |client, data, match|
-      line = File.read("db/txt/#{hook}.txt").split("\n").sample
+      line = File.read("#{File.dirname(__FILE__)}/../db/txt/#{hook}.txt").split("\n").sample
       line.gsub!('$args', match[:args]) if match[:args]
       line.gsub!('$source', find_user(client, data.user)['name'])
       client.message text: line, channel: data.channel
