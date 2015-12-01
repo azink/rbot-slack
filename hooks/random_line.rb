5adf3921f8d9b47ccc92689c848fece531c3d38e
Aaron Zink <azink@solsar.com>
2015-12-01 11:45:00 -0800

    Fixing random line directory

--- hooks/random_line.rb
+++ hooks/random_line.rb
@@ -1,7 +1,7 @@
 class RandomLine < SlackRubyBot::Commands::Base
   # Reads random lines
   # Dynamically reads db/txt/*.txt on startup and generates hooks. Use symlinks for aliases.
-  hooks = Dir["./db/txt/*.txt"].map {|f| f[/\/(\w+)\./, 1]}
+  hooks = Dir["#{File.dirname(__FILE__)}/../db/txt/*.txt"].map {|f| f[/\/(\w+)\./, 1]}
   hooks.each do |hook|
     match(/^!#{hook}($|\s(?<args>.+$))/i) do |client, data, match|
       line = File.read("#{File.dirname(__FILE__)}/../db/txt/#{hook}.txt").split("\n").sample
