c3dc7f324eaa28fa863531ab2bb6b9f31bf43abd
Aaron Zink <azink@solsar.com>
2015-12-03 14:58:47 -0800

    Don't resolve IRC bot usernames

--- lib/message_handler.rb
+++ lib/message_handler.rb
@@ -1,20 +1,20 @@
 # Applies to all messages
 class MessageHandler < SlackRubyBot::Commands::Base
   match_all do |client, data, match|
-    # update last table for !seen
     user = find_user(client, data.user)
     username = user.nil? ? data.user : user['name']
     unless username.nil?
+      # update last table for !seen
       DB['INSERT OR REPLACE INTO last VALUES(?, ?, ?, ?);', username, data.channel, data.text, Time.now.to_i].all
-    end
 
-    # write to log
-    dir = "#{$rbot_root}/log/#{data.channel}"
-    t = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
-    log = "#{dir}/#{Time.now.utc.strftime('%Y-%m-%d')}-#{data.channel}.log"
-    Dir.mkdir(dir) unless File.directory?(dir)
-    File.open(log, "a+") do |f|
-      f.puts("#{t} <#{find_user(client, data.user)['name']}> #{data.text}")
+      # write to log
+      dir = "#{$rbot_root}/log/#{data.channel}"
+      t = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+      log = "#{dir}/#{Time.now.utc.strftime('%Y-%m-%d')}-#{data.channel}.log"
+      Dir.mkdir(dir) unless File.directory?(dir)
+      File.open(log, "a+") do |f|
+        f.puts("#{t} <#{username}> #{data.text}")
+      end
     end
   end
 end
