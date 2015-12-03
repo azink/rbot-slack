09185c012f05ad41338d510adcc4506c0dabf010
Aaron Zink <azink@solsar.com>
2015-12-03 14:44:12 -0800

    fix seen

--- hooks/seen.rb
+++ hooks/seen.rb
@@ -37,7 +37,7 @@ class Seen < SlackRubyBot::Commands::Base
   end
 
   match(/^!seen\s(?<args>.+$)/i) do |client, data, match|
-    row = DB[:last].where(Sequel.like(:nick, "%#{match[:args].delete("@")}%")).first
+    row = DB[:last].where(:nick => match[:args].delete("@")).first
     msg =
       if row
         format_seen(row)
