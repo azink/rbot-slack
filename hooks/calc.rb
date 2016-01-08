592d30578eb3c75039dd14fd837a1b614ba3065a
Aaron Zink <azink@solsar.com>
2016-01-07 20:32:29 -0800

    handle calc errors

--- hooks/calc.rb
+++ hooks/calc.rb
@@ -2,6 +2,7 @@
 class Calc < SlackRubyBot::Commands::Base
   match(/^!calc($|\s(?<args>.+$))/i) do |client, data, match|
     out = `echo "#{match[:args]}" | bc -l 2>&1`
+    out = "Calc error. Try !conv <experssion> instead.  Error: #{out}" if out =~ /error/
     client.message text: out, channel: data.channel
   end
 end
