f97bbb05bf67ae34ff2256ab296b4b152049337e
Aaron Zink <azink@solsar.com>
2020-04-02 15:20:02 -0700

    changing APIs for stock

--- hooks/stock.rb
+++ hooks/stock.rb
@@ -2,23 +2,41 @@
 class Stock < SlackRubyBot::Commands::Base
   match(/^!stock($|\s(?<args>.+$))/i) do |client, data, match|
     require 'json'
-    info = JSON.parse(open("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=#{match[:args]}&interval=1min&apikey=#{ENV['ALPHA_KEY']}").read)
-    if (info['Meta Data'])
-      s_name = info['Meta Data']['2. Symbol'].upcase
-      today = info['Time Series (Daily)'].shift[1]
-      yesterday = info['Time Series (Daily)'].shift[1]
-      y_close = yesterday['4. close'].to_f
-      s_close = today['4. close'].to_f
-      s_high = today['2. high'].to_f
-      s_low = today['3. low'].to_f
-      s_change = (s_close - y_close).round(2)
-      s_pct = ((s_change / y_close) * 100).round(2)
-      s_change = "+#{s_change}" if s_change > 0
-      s_pct = "+#{s_pct}" if s_pct > 0
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
 
-      out = "#{s_name} - Last: #{s_close}, (#{s_change}, #{s_pct}%); high: #{s_high}, low: #{s_low}"
-    else
+    url = URI("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=#{match[:args]}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+    request["x-rapidapi-key"] = ENV['RAPID_API_KEY']
+
+    response = http.request(request)
+    info = JSON.parse(response.read_body)
+
+    if (info['quoteResponse']['result'].empty?)
       out = "Error parsing quote for #{match[:args]}. Check the symbol and try again."
+    else
+      ticker = info['quoteResponse']['result'][0]
+
+      s_symbol = ticker['symbol']
+      s_name = ticker['shortName']
+      s_exchange = ticker['fullExchangeName']
+      s_price = ticker['regularMarketPrice']
+      s_change = ticker['regularMarketChange'].round(2)
+      s_change_pct = ticker['regularMarketChangePercent'].round(2)
+      s_high = ticker['regularMarketDayHigh']
+      s_low = ticker['regularMarketDayLow']
+      a_price = ticker['postMarketPrice']
+      a_change = ticker['postMarketChange'].round(2)
+      a_change_pct = ticker['postMarketChangePercent'].round(2)
+
+      out = "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}. Post-market: #{a_price}, (#{a_change}, #{a_change_pct}%)"
     end
     client.message text: out, channel: data.channel
   end
