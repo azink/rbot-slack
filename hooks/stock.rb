3902ad986631a4a87dae3eb544914c73012c4acf
Aaron Zink <azink@solsar.com>
2020-04-08 08:59:02 -0700

    multiple stocks

--- hooks/stock.rb
+++ hooks/stock.rb
@@ -18,28 +18,29 @@ class Stock < SlackRubyBot::Commands::Base
 
     response = http.request(request)
     info = JSON.parse(response.read_body)
+    out = ""
 
     if (info['quoteResponse']['result'].empty?)
       out = "Error parsing quote for #{match[:args]}. Check the symbol and try again."
     else
-      ticker = info['quoteResponse']['result'][0]
-
-      s_symbol = ticker['symbol']
-      s_name = ticker['shortName']
-      s_exchange = ticker['fullExchangeName']
-      s_price = ticker['regularMarketPrice']
-      s_change = ticker['regularMarketChange'].round(2)
-      s_change_pct = ticker['regularMarketChangePercent'].round(2)
-      s_high = ticker['regularMarketDayHigh']
-      s_low = ticker['regularMarketDayLow']
-      if (ticker['postMarketPrice'])
-	      a_price = ticker['postMarketPrice']
-  	    a_change = ticker['postMarketChange'].round(2)
-    	  a_change_pct = ticker['postMarketChangePercent'].round(2)
-			  out = "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}. Post-market: #{a_price}, (#{a_change}, #{a_change_pct}%)"
-      else
-				out = "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}"
-    	end
+      info['quoteResponse']['result'].each do |ticker|
+        s_symbol = ticker['symbol']
+        s_name = ticker['shortName']
+        s_exchange = ticker['fullExchangeName']
+        s_price = ticker['regularMarketPrice']
+        s_change = ticker['regularMarketChange'].round(2)
+        s_change_pct = ticker['regularMarketChangePercent'].round(2)
+        s_high = ticker['regularMarketDayHigh']
+        s_low = ticker['regularMarketDayLow']
+        if (ticker['postMarketPrice'])
+          a_price = ticker['postMarketPrice']
+          a_change = ticker['postMarketChange'].round(2)
+          a_change_pct = ticker['postMarketChangePercent'].round(2)
+          out += "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}. Post-market: #{a_price}, (#{a_change}, #{a_change_pct}%)\n"
+        else
+          out += "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}\n"
+        end
+      end
     end
     client.message text: out, channel: data.channel
   end
