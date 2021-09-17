1817d2ba34a5dca7880786b53925c37893ba8c9b
Aaron Zink <azink@solsar.com>
2021-09-17 08:59:10 -0700

    renaming stock

---
+++ hooks/stock-old.rb
@@ -0,0 +1,47 @@
+# Stocks
+class Stock < SlackRubyBot::Commands::Base
+  match(/^!stock-old($|\s(?<args>.+$))/i) do |client, data, match|
+    require 'json'
+    require 'uri'
+    require 'net/http'
+    require 'openssl'
+
+    url = URI("https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-quotes?region=US&lang=en&symbols=#{match[:args]}")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-host"] = 'apidojo-yahoo-finance-v1.p.rapidapi.com'
+    request["x-rapidapi-key"] = ENV['RAPID_API_KEY']
+
+    response = http.request(request)
+    info = JSON.parse(response.read_body)
+    out = ""
+
+    if (info['quoteResponse']['result'].empty?)
+      out = "Error parsing quote for #{match[:args]}. Check the symbol and try again."
+    else
+      info['quoteResponse']['result'].each do |ticker|
+        s_symbol = ticker['symbol']
+        s_name = ticker['shortName']
+        s_exchange = ticker['fullExchangeName']
+        s_price = ticker['regularMarketPrice']
+        s_change = ticker['regularMarketChange'].round(2)
+        s_change_pct = ticker['regularMarketChangePercent'].round(2)
+        s_high = ticker['regularMarketDayHigh']
+        s_low = ticker['regularMarketDayLow']
+        if (ticker['postMarketPrice'])
+          a_price = ticker['postMarketPrice']
+          a_change = ticker['postMarketChange'].round(2)
+          a_change_pct = ticker['postMarketChangePercent'].round(2)
+          out += "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}. Post-market: #{a_price}, (#{a_change}, #{a_change_pct}%)\n"
+        else
+          out += "#{s_name} (#{s_symbol}, #{s_exchange}) - Last close: #{s_price}, (#{s_change}, #{s_change_pct}%); high: #{s_high}, low: #{s_low}\n"
+        end
+      end
+    end
+    client.message text: out, channel: data.channel
+  end
+end
